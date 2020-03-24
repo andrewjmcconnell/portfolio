be9f44d43c2c6fbdb1e4d65633279fb756b48563
andrewjmcconnell <andrewjmcconnell93@gmail.com>
2020-03-24 00:13:45 -0500

    working on a seperate menu for mobile

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { withRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, ThemeContext } from "styled-components";
 
 import face from "tabler-icons/icons/face-id.svg";
 import exit from "tabler-icons/icons/corner-down-left.svg";
@@ -25,37 +25,78 @@ import {
   Bottom
 } from "./Menu.styled";
 
+import {
+  MobileContainer,
+  MobileIcon,
+  MobileBar,
+  DropdownBar,
+  MobileButtonLink,
+  MobileButton,
+  MobileImg
+} from "./Menu.mobile.styled";
+
 const Menu = ({ location, theme, toggleTheme }) => {
+  const { isMobile } = useContext(ThemeContext);
+
   const home = location.pathname === "/";
   const [close, setClose] = useState(false);
+
+  const DesktopMenu = (
+    <Base>
+      <MenuDiv onClick={() => setClose(!close)}>
+        <Icon>
+          <Bar />
+        </Icon>
+      </MenuDiv>
+      <Icons>
+        <ButtonOne src={home ? face : exit} alt="" />
+        <ButtonTwo src={theme ? sun : moon} alt="" />
+        <ButtonThree src={mail} alt="" />
+      </Icons>
+      <Section>
+        <CoverOne>
+          <CoverTwo>
+            <Content
+              onClick={() => {
+                toggleTheme(!theme);
+                localStorage.setItem("theme", theme);
+              }}
+            />
+          </CoverTwo>
+        </CoverOne>
+        <Top to={home ? "/about" : "/"}></Top>
+        <Bottom onClick={() => {}} />
+      </Section>
+    </Base>
+  );
+
+  const MobileMenu = (
+    <MobileContainer>
+      <MobileIcon onClick={() => setClose(!close)}>
+        <MobileBar />
+      </MobileIcon>
+      <DropdownBar>
+        <MobileButtonLink to={home ? "/about" : "/"}>
+          <MobileImg src={home ? face : exit} alt="" />
+        </MobileButtonLink>
+        <MobileButton
+          onClick={() => {
+            toggleTheme(!theme);
+            localStorage.setItem("theme", theme);
+          }}
+        >
+          <MobileImg src={theme ? sun : moon} alt="" />
+        </MobileButton>
+        <MobileButton onClick={() => {}}>
+          <MobileImg src={mail} alt="" />
+        </MobileButton>
+      </DropdownBar>
+    </MobileContainer>
+  );
+
   return (
     <ThemeProvider theme={{ close: close }}>
-      <Base>
-        <MenuDiv onClick={() => setClose(!close)}>
-          <Icon>
-            <Bar />
-          </Icon>
-        </MenuDiv>
-        <Icons>
-          <ButtonOne src={home ? face : exit} alt="" />
-          <ButtonTwo src={theme ? sun : moon} alt="" />
-          <ButtonThree src={mail} alt="" />
-        </Icons>
-        <Section>
-          <CoverOne>
-            <CoverTwo>
-              <Content
-                onClick={() => {
-                  toggleTheme(!theme);
-                  localStorage.setItem("theme", theme);
-                }}
-              />
-            </CoverTwo>
-          </CoverOne>
-          <Top to={home ? "/about" : "/"}></Top>
-          <Bottom onClick={() => {}} />
-        </Section>
-      </Base>
+      {isMobile ? MobileMenu : DesktopMenu}
     </ThemeProvider>
   );
 };
---
+++ src/components/Menu/Menu.mobile.styled.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const MobileContainer = styled.div`
+  display: block;
+  position: fixed;
+  margin: 0 auto;
+  z-index: 1;
+
+  width: 100%;
+`;
+
+export const MobileIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 40px;
+  background-color: white;
+
+  &:before,
+  &:after {
+    content: "";
+    display: inline-block;
+    width: 30px;
+    height: 3px;
+    background-color: #333;
+    border-radius: 3px;
+    transition: 0.4s;
+    transform: rotate(0deg);
+  }
+
+  &:before {
+    transform: translate(15px, 8px);
+
+    ${({ theme }) =>
+      theme.close &&
+      "transform: translate(15px, 18px) rotate(-45deg); opacity: 1;"}
+  }
+
+  &:after {
+    transform: translate(-15px, 28px);
+    ${({ theme }) =>
+      theme.close &&
+      "transform: translate(-15px, 18px) rotate(45deg); opacity: 1;"}
+  }
+`;
+
+export const MobileBar = styled.div`
+  position: absolute;
+  top: 18px;
+  width: 30px;
+  height: 3px;
+  background-color: #333;
+  border-radius: 3px;
+  transition: 0.4s;
+
+  ${({ theme }) => theme.close && "opacity: 0;"}
+`;
+
+export const DropdownBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background-color: transparent;
+`;
+
+export const MobileButtonLink = styled(NavLink)`
+  margin-top: 0.5rem;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  background-color: white;
+  opacity: 0;
+  transition: 0.4s;
+  transform: translate(0, -5rem);
+
+  ${({ theme }) =>
+    theme.close && "transform: translate(0, 0.5rem); opacity: 1;"}
+`;
+
+export const MobileButton = styled.button`
+  padding: 0;
+  margin-top: 0.5rem;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  background-color: white;
+  opacity: 0;
+  transition: 0.4s;
+  transform: translate(0, -5rem);
+
+  ${({ theme }) =>
+    theme.close && "transform: translate(0, 0.5rem); opacity: 1;"}
+`;
+
+export const MobileImg = styled.img`
+  top: 50%;
+  transform: translate(0.25rem);
+`;

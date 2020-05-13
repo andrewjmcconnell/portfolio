b7b783e7707f42226d777bc9711c5f88587613ea
andrewjmcconnell <andrewjmcconnell93@gmail.com>
2020-05-13 00:00:28 -0500

    fixed Beach Day waves

--- src/animations/Waves/Waves.styled.js
+++ src/animations/Waves/Waves.styled.js
@@ -1,23 +1,70 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const move = keyframes`
+0% {
+    transform: scale(1, 1);
+}
+50% {
+    transform: scale(1, 1.2) skewX(25deg);
+}
+100%{
+    transform: scale(1, 1);
+}
+`;
 
 export const WavesWrapper = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 15%;
-    top: ${({ top }) => top}%;
-    bottom: ${({ bottom }) => bottom}%;
+  position: absolute;
+  width: 110%;
+  height: 15%;
+  top: ${({ top }) => top}%;
+  bottom: ${({ bottom }) => bottom}%;
 `;
 
 export const Wave = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 85%;
+  background-color: #84cfc4;
+  animation: ${move} 6s infinite;
+
+  &:before {
+    content: "";
     position: absolute;
-    width: 100%;
-    height: 85%;
-    background-color: #84CFC4;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-repeat: repeat-x;
+
+    height: 1vh;
+    background-size: 2vh 2vh;
+    background-image: radial-gradient(
+      circle at 1vh -0.5vh,
+      transparent 1.2vh,
+      white 1.3vh
+    );
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-repeat: repeat-x;
+
+    height: 1.5vh;
+    background-size: 4vh 2vh;
+    background-image: radial-gradient(
+      circle at 1vh 1.5vh,
+      white 1.2vh,
+      transparent 1.3vh
+    );
+  }
 `;
 
 export const Foam = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: white;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: white;
 `;

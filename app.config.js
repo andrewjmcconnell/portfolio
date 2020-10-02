84e351abf6f752420f3f1dff3268cc655fe8f6bf
andrewjmcconnell <andrewjmcconnell93@gmail.com>
2020-10-02 09:56:30 -0500

    fix

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name       : "client",
-    script     : "serve -s build",
+    script     : "yarn start",
     watch       : true,
     env: {
       "NODE_ENV": "development",
